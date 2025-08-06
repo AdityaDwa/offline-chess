a4bc74ce7cab95fd8254519d8f233d18b0f2a12b
Aditya Dwa <adeetyadwa011@gmail.com>
2025-08-06 15:55:42 +0545

    feat/board-render Define data structure for each piece object

---
+++ src/pieces_info.js
@@ -0,0 +1,335 @@
+import blackPawn from "./assets/black_pawn.png";
+import blackRook from "./assets/black_rook.png";
+import blackKnight from "./assets/black_knight.png";
+import blackBishop from "./assets/black_bishop.png";
+import blackQueen from "./assets/black_queen.png";
+import blackKing from "./assets/black_king.png";
+import whitePawn from "./assets/white_pawn.png";
+import whiteRook from "./assets/white_rook.png";
+import whiteKnight from "./assets/white_knight.png";
+import whiteBishop from "./assets/white_bishop.png";
+import whiteQueen from "./assets/white_queen.png";
+import whiteKing from "./assets/white_king.png";
+
+export const PIECES_INFO = [
+  {
+    id: "bp1",
+    title: "black-rook",
+    position: {
+      row: 0,
+      col: 0,
+    },
+    moveList: [],
+    src: blackRook,
+  },
+  {
+    id: "bp2",
+    title: "black-knight",
+    position: {
+      row: 0,
+      col: 1,
+    },
+    moveList: [],
+    src: blackKnight,
+  },
+  {
+    id: "bp3",
+    title: "black-bishop",
+    position: {
+      row: 0,
+      col: 2,
+    },
+    moveList: [],
+    src: blackBishop,
+  },
+  {
+    id: "bp4",
+    title: "black-queen",
+    position: {
+      row: 0,
+      col: 3,
+    },
+    moveList: [],
+    src: blackQueen,
+  },
+  {
+    id: "bp5",
+    title: "black-king",
+    position: {
+      row: 0,
+      col: 4,
+    },
+    moveList: [],
+    src: blackKing,
+  },
+  {
+    id: "bp6",
+    title: "black-bishop",
+    position: {
+      row: 0,
+      col: 5,
+    },
+    moveList: [],
+    src: blackBishop,
+  },
+  {
+    id: "bp7",
+    title: "black-knight",
+    position: {
+      row: 0,
+      col: 6,
+    },
+    moveList: [],
+    src: blackKnight,
+  },
+  {
+    id: "bp8",
+    title: "black-rook",
+    position: {
+      row: 0,
+      col: 7,
+    },
+    moveList: [],
+    src: blackRook,
+  },
+  {
+    id: "bp9",
+    title: "black-pawn",
+    position: {
+      row: 1,
+      col: 0,
+    },
+    moveList: [],
+    src: blackPawn,
+  },
+  {
+    id: "bp10",
+    title: "black-pawn",
+    position: {
+      row: 1,
+      col: 1,
+    },
+    moveList: [],
+    src: blackPawn,
+  },
+  {
+    id: "bp11",
+    title: "black-pawn",
+    position: {
+      row: 1,
+      col: 2,
+    },
+    moveList: [],
+    src: blackPawn,
+  },
+  {
+    id: "bp12",
+    title: "black-pawn",
+    position: {
+      row: 1,
+      col: 3,
+    },
+    moveList: [],
+    src: blackPawn,
+  },
+  {
+    id: "bp13",
+    title: "black-pawn",
+    position: {
+      row: 1,
+      col: 4,
+    },
+    moveList: [],
+    src: blackPawn,
+  },
+  {
+    id: "bp14",
+    title: "black-pawn",
+    position: {
+      row: 1,
+      col: 5,
+    },
+    moveList: [],
+    src: blackPawn,
+  },
+  {
+    id: "bp15",
+    title: "black-pawn",
+    position: {
+      row: 1,
+      col: 6,
+    },
+    moveList: [],
+    src: blackPawn,
+  },
+  {
+    id: "bp16",
+    title: "black-pawn",
+    position: {
+      row: 1,
+      col: 7,
+    },
+    moveList: [],
+    src: blackPawn,
+  },
+  {
+    id: "wp1",
+    title: "white-pawn",
+    position: {
+      row: 6,
+      col: 0,
+    },
+    moveList: [],
+    src: whitePawn,
+  },
+  {
+    id: "wp2",
+    title: "white-pawn",
+    position: {
+      row: 6,
+      col: 1,
+    },
+    moveList: [],
+    src: whitePawn,
+  },
+  {
+    id: "wp3",
+    title: "white-pawn",
+    position: {
+      row: 6,
+      col: 2,
+    },
+    moveList: [],
+    src: whitePawn,
+  },
+  {
+    id: "wp4",
+    title: "white-pawn",
+    position: {
+      row: 6,
+      col: 3,
+    },
+    moveList: [],
+    src: whitePawn,
+  },
+  {
+    id: "wp5",
+    title: "white-pawn",
+    position: {
+      row: 6,
+      col: 4,
+    },
+    moveList: [],
+    src: whitePawn,
+  },
+  {
+    id: "wp6",
+    title: "white-pawn",
+    position: {
+      row: 6,
+      col: 5,
+    },
+    moveList: [],
+    src: whitePawn,
+  },
+  {
+    id: "wp7",
+    title: "white-pawn",
+    position: {
+      row: 6,
+      col: 6,
+    },
+    moveList: [],
+    src: whitePawn,
+  },
+  {
+    id: "wp8",
+    title: "white-pawn",
+    position: {
+      row: 6,
+      col: 7,
+    },
+    moveList: [],
+    src: whitePawn,
+  },
+  {
+    id: "wp9",
+    title: "white-rook",
+    position: {
+      row: 7,
+      col: 0,
+    },
+    moveList: [],
+    src: whiteRook,
+  },
+  {
+    id: "wp10",
+    title: "white-knight",
+    position: {
+      row: 7,
+      col: 1,
+    },
+    moveList: [],
+    src: whiteKnight,
+  },
+  {
+    id: "wp11",
+    title: "white-bishop",
+    position: {
+      row: 7,
+      col: 2,
+    },
+    moveList: [],
+    src: whiteBishop,
+  },
+  {
+    id: "wp12",
+    title: "white-queen",
+    position: {
+      row: 7,
+      col: 3,
+    },
+    moveList: [],
+    src: whiteQueen,
+  },
+  {
+    id: "wp13",
+    title: "white-king",
+    position: {
+      row: 7,
+      col: 4,
+    },
+    moveList: [],
+    src: whiteKing,
+  },
+  {
+    id: "wp14",
+    title: "white-bishop",
+    position: {
+      row: 7,
+      col: 5,
+    },
+    moveList: [],
+    src: whiteBishop,
+  },
+  {
+    id: "wp15",
+    title: "white-knight",
+    position: {
+      row: 7,
+      col: 6,
+    },
+    moveList: [],
+    src: whiteKnight,
+  },
+  {
+    id: "wp16",
+    title: "white-rook",
+    position: {
+      row: 7,
+      col: 7,
+    },
+    moveList: [],
+    src: whiteRook,
+  },
+];
